f43b7b7700bffc448a6402fb949db011157a551c
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-03 16:09:50 +0200

    Brew formula update for api-scenario version v0.0.1

--- api-scenario.rb
+++ api-scenario.rb
@@ -7,19 +7,19 @@ class ApiScenario < Formula
 
   if OS.mac?
     url "https://github.com/thomaspoignant/api-scenario/releases/download/v0.0.1/api-scenario_0.0.1_Darwin_x86_64.tar.gz"
-    sha256 "d084091f6793bc4c44c4749aa7492b596aafd4aa4db6d44b992f2e3c0be48732"
+    sha256 "07957f6e39da82479b253a70f68e0a617d699eed8b00e188ad4036059b4a8bf7"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/thomaspoignant/api-scenario/releases/download/v0.0.1/api-scenario_0.0.1_Linux_x86_64.tar.gz"
-      sha256 "b314af3ab98de2b4ce2dccce2b3b40dfaa3dfe2a0230bb38c3cbba2819d6207d"
+      sha256 "edf761ca28da0e27431a4f4d5f8e426ff789cf4c67409c4c46f21fb120509d94"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
         url "https://github.com/thomaspoignant/api-scenario/releases/download/v0.0.1/api-scenario_0.0.1_Linux_arm64.tar.gz"
-        sha256 "85c27bc31c391cd629f064355027bf45b6f50e568996146bd9317417a5e9f8dd"
+        sha256 "01b23721eb7b0271155be528f95d70b7c1ea79cc0b71c624ec321678254824f6"
       else
         url "https://github.com/thomaspoignant/api-scenario/releases/download/v0.0.1/api-scenario_0.0.1_Linux_armv6.tar.gz"
-        sha256 "b352df0ed68475c6ccf8cc7ea06739f324a01dc8c7cfe06a807b732de21cdaff"
+        sha256 "20a8d2bad7d1c37e3d517987bd906e17f356de52eb608906ef9e49eed4aef1d1"
       end
     end
   end
